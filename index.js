d9e22e36c5627645bfd419fd4d05efd55da1e516
MarioBahaa <32718005+Mariobahaa@users.noreply.github.com>
2021-02-23 21:38:54 +0200

    Post User Data

---
+++ index.js
@@ -0,0 +1,40 @@
+const { json } = require('body-parser');
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+
+app.use(express.json());
+//app.use(express.urlencoded());
+
+app.post('/profile',(req,res)=>{
+    //console.log(req);
+    let newUser = req.body;
+    //console.log(newUser);
+
+    fs.readFile('./users.txt',(err,data)=>{
+        let usersList = [];
+        if(err!= null && err!=undefined){
+            console.log(err.message);
+        }
+        else{
+            usersList = JSON.parse(data);
+        }
+        usersList.push(newUser);
+        fs.writeFile('./users.txt',JSON.stringify(usersList), (err,data)=>{
+            if(err!= null && err!=undefined){
+                console.log(err.message);
+            }    
+            else{
+                console.log('saved successfully');
+            }
+        });
+    })
+
+    res.send('Saved');
+})
+
+
+app.listen(3000,()=>{
+    console.log('listening');
+});
