94d036a3f96a535a126d6d0d37299c5d9a4f5434
MarioBahaa <32718005+Mariobahaa@users.noreply.github.com>
2021-02-23 23:21:03 +0200

    Get with param

--- index.js
+++ index.js
@@ -32,8 +32,55 @@ app.post('/profile',(req,res)=>{
 
             }
         });
-    })
+    });
 
+});
+
+app.get('/profile',(req,res)=>{
+    res.contentType('html');
+    res.write('<link href="/style" rel ="stylesheet" type="text/css">');
+    res.write('<img src="/myimage"/>');
+
+    //console.log(req.url);
+    //console.log(req.query);
+    const query = req.query;
+    fs.readFile('./users.txt',(err,data)=>{
+        let usersList = [];
+        if(err!= null && err!=undefined){
+            console.log(err.message);
+        }
+        else{
+            usersList = JSON.parse(data);
+        }
+        let user = [];
+        if(query.id != null && query.id != undefined)
+            user = usersList.filter(u => u.id == parseInt( query.id ));
+        else if(query.name != null && query.name != undefined)
+            user = usersList.filter(u => u.name == query.name);
+        else if(query.age != null && query.age != undefined)
+            user = usersList.filter(u => u.age == parseInt( query.age ));
+        else if(query.phone != null && query.phone != undefined)
+            user = usersList.filter(u => u.phone == query.phone);
+        
+        // console.log(user);
+        // console.log(query);
+        user.forEach(u => {
+            console.log(u.id);
+            
+            res.write(`<h3>id = ${u.id}</h3>`);
+            res.write(`<h3>username = ${u.name}</h3>`);
+            res.write(`<h3>age = ${u.age}</h3>`);
+            res.write(`<h3>phone = ${u.phone}</h3>`);
+
+        });
+
+        //res.send('');
+        res.end('<br>');
+        
+    });
+
+  
+    //res.end('');
 });
 
 app.get('/myimage',(req,res)=>{
@@ -44,6 +91,10 @@ app.get('/style',(req,res)=>{
     res.sendFile(__dirname + "\\style.css");
 });
 
+app.use("*", function (req, res) {
+    res.sendStatus(404);
+    res.send("not matched");   
+});
 
 app.listen(3000,()=>{
     console.log('listening');
